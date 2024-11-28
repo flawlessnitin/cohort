c42dd430cd852a00a9765084fdb95036af60e001
Nitin Sahu <sahunitin970@gmail.com>
2024-11-29 05:01:33 +0530

    Week 2 assignment working

--- week-2-assignment-2/file/todo.js
+++ week-2-assignment-2/file/todo.js
@@ -1,6 +1,6 @@
 const todo = [{
   id: "flawlessnitin",
-  title: "flawlessnitinnnnn",
+  title: "flawlessnitinnnnnnnnn",
   description: "I'm Nitin Sahu. Full Stack Developer from India!!!"
 }];
 module.exports = todo;
