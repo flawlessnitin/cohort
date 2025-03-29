bdb3a63b2ac58785c0ffe04f9db080bbc858039d
Nitin Sahu <sahunitin970@gmail.com>
2025-03-29 17:21:42 +0530

    Add OnRamp transaction functionality to AddMoney component

---
+++ week-18/week-17-final-code/apps/user-app/app/lib/actions/createOnRamptnx.ts
@@ -0,0 +1,31 @@
+"use server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import prisma from "@repo/db/client";
+
+export async function createOnRampTransaction(
+  amount: number,
+  provider: string
+) {
+  const session = await getServerSession(authOptions);
+  const token = Math.random().toString(36).substring(2, 15);
+  const userId = session?.user.id;
+  if (!userId) {
+    return {
+      message: "User not logged in",
+    };
+  }
+  prisma.onRampTransaction.create({
+    data: {
+      userId,
+      amount: amount,
+      status: "Processing",
+      startTime: new Date(),
+      provider,
+      token,
+    },
+  });
+  return {
+    message: "Transaction created",
+  };
+}
--- week-18/week-17-final-code/apps/user-app/components/AddMoneyCard.tsx
+++ week-18/week-17-final-code/apps/user-app/components/AddMoneyCard.tsx
@@ -5,6 +5,7 @@ import { Center } from "@repo/ui/center";
 import { Select } from "@repo/ui/select";
 import { useState } from "react";
 import { TextInput } from "@repo/ui/textinput";
+import { OnRampTransactions } from "./OnRampTransactions";
 
 const SUPPORTED_BANKS = [{
     name: "HDFC Bank",
@@ -16,6 +17,8 @@ const SUPPORTED_BANKS = [{
 
 export const AddMoney = () => {
     const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
+    const [amount, setAmount] = useState(0);
+    const [provider, setProvider] = useState(SUPPORTED_BANKS[0]?.name);
     return <Card title="Add Money">
     <div className="w-full">
         <TextInput label={"Amount"} placeholder={"Amount"} onChange={() => {
@@ -26,12 +29,14 @@ export const AddMoney = () => {
         </div>
         <Select onSelect={(value) => {
             setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "")
+            setProvider(SUPPORTED_BANKS.find(x => x.name === value)?.name || "")
         }} options={SUPPORTED_BANKS.map(x => ({
             key: x.name,
             value: x.name
         }))} />
         <div className="flex justify-center pt-4">
             <Button onClick={() => {
+              OnRampTransactions(amount, provider)
                 window.location.href = redirectUrl || "";
             }}>
             Add Money
